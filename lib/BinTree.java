da0217376bda95471a1e3bc1910e37762f827bab
LeaderKam <48421007+LeaderKam@users.noreply.github.com>
2020-01-26 23:03:18 +0100

    modif regex

--- lib/BinTree.java
+++ lib/BinTree.java
@@ -19,9 +19,9 @@ public class BinTree {
 	/**
 	 * Constructor of the BinTree class.
 	 * 
-	 * @param {*} data : Value of the node.
-	 * @param {*} leftTree : data's left BinTree.
-	 * @param {*} rightTree : data's right BinTree.
+	 * @param data : Value of the node.
+	 * @param leftTree : data's left BinTree.
+	 * @param rightTree : data's right BinTree.
 	 */
 	public BinTree(String data, BinTree leftTree, BinTree rightTree) {
 		this.data = data;
@@ -38,61 +38,49 @@ public class BinTree {
 	 * @return data
 	 */
 	public String getData() {
-		return data;
-		
+		return data;	
 	}
 
 	/**
 	 * @param data
 	 */
 	public void setData(String data) {
-		this.data = data;
-		
+		this.data = data;	
 	}
 
 	/**
 	 * @return left BinTree
 	 */
 	public BinTree getLeft() {
-		return left;
-		
+		return left;	
 	}
 
 	/**
 	 * @param leftTree
 	 */
 	public void setLeft(BinTree leftTree) {
-
 		if (leftTree.getData() != "nil") {
 			this.left = leftTree;
-		}
-		
+		}	
 	}
 
 	/**
 	 * @return right BinTree
 	 */
 	public BinTree getRight() {
-		return right;
-		
+		return right;	
 	}
 
 	public void setRight(BinTree rightTree) {
-
 		if (rightTree.getData() != "nil") {
 			this.right = rightTree;
 		}
 	}
 
-	/*
-	 * var args = []; args.push(new BinTree("nil",null,null)); args.push(new
-	 * BinTree("nil",null,null)); args.push(new BinTree("nil",null,null));
-	 * args.push(new BinTree("nil",null,null)); var bt = cons(args);
-	 * console.log(bt); console.log(bt.toString());
-	 */
+	
 
 	/**
-	 * @param {*} tree : BinTree object.
+	 * @param tree : BinTree object.
 	 * @returns the head of tree. If its left son equals to null, will return null.
 	 */
 	public BinTree head(BinTree tree) {
@@ -103,8 +91,8 @@ public class BinTree {
 	};
 
 	/**
-	 * @param {*} tree : BinTree object.
-	 * @returns the tail of tree. If its right son equals to null, will return null.
+	 * @param tree : BinTree object.
+	 * @return Bintree: the tail of tree. If its right son equals to null, will return null.
 	 */
 	public BinTree tail(BinTree tree) {
 		if (tree.getRight() != null) {
@@ -114,14 +102,8 @@ public class BinTree {
 	};
 
 	/**
-	 * If the array contains less than 2 objects, it will produce a (nil) BinTree.
-	 * Else : The BinTree's root node is a cons, with the first element has a left
-	 * son and another cons has a right son. The last cons contains the last 2
-	 * elements of the array. Ex : array = {nil, nil, nil}. cons(array) will produce
-	 * : (cons nil (cons nil nil)).
-	 * 
-	 * @param {*} args : array<BinTree>
-	 * @returns BinTree object with the BinTrees in args.
+	 * @param args : LinkedList<BinTree>
+	 * @return BinTree object
 	 */
 	public BinTree cons(LinkedList<BinTree> args) {
 		if (args != null) {
@@ -129,48 +111,48 @@ public class BinTree {
 				return new BinTree("nil", null, null);
 			}
 
-			BinTree tree = args.pop(); // retourne le premier element du tableau
+			BinTree tree = args.pop(); // return the first element of the linkedlist
 			if (args.size() <= 0) {
 				return tree;
 			} else {
 				return new BinTree("cons", tree, cons(args));
 			}
 		}
-		return left;//revoir
+		return null;
 	};
 
 	/**
 	 * Same as cons function but we have lists instead of cons, and we have an
 	 * additional (nil) at the end of the BinTree.
 	 * 
-	 * @param {*} args : array<BinTree>
-	 * @returns List of BinTrees from a given array of BinTrees. (nil) if it has
+	 * @param args : LinkedList<BinTree>
+	 * @return List of BinTrees from a given array of BinTrees. (nil) if it has
 	 *          less than 2 elements.
 	 */
-	public BinTree list(List<BinTree> args) {
+	public BinTree list(LinkedList<BinTree> args) {
 		if (args != null) {
 			if (args.size() == 0) {
 				return new BinTree("nil", null, null);
 			}
 
-			BinTree tree = args.get(0);
+			BinTree tree = args.pop();
 			if (args.size() <= 0) {
 				return new BinTree("list", tree, new BinTree("nil", null, null));
 			} else {
 				return new BinTree("list", tree, list(args));
 			}
 		}
-		return null;//revoir
+		return null;
 	};
 
 	/**
 	 * Apply the given operand to the trees.
-	 * @param {} operand : Given operand, can be either AND, OR or EQ
-	 * @param {*} tree1 : BinTree object (left side).
-	 * @param {*} tree2 : BinTree object (right side).
-	 * @returns case AND : (nil) if one of the elements equals to (nil), (cons(nil)(nil)) else.
-	 *          case OR : (nil) if both of the elements are equal to (nil), (cons(nil)(nil)) else.
-	 *          case EQ : (nil) if the elements are not equal, (cons(nil)(nil)) else.
+	 * @param operand : Given operand, can be either AND, OR or EQ
+	 * @param tree1 : BinTree object (left side).
+	 * @param tree2 : BinTree object (right side).
+	 * @return case AND : (nil) if one of the elements equals to (nil), (cons nil nil) else.
+	 *          case OR : (nil) if both of the elements are equal to (nil), (cons nil nil) else.
+	 *          case EQ : (nil) if the elements are not equal, (cons nil nil) else.
 	 */
 	public BinTree evaluate(String operand,BinTree tree1,BinTree tree2) {
 	   if (operand == "AND")
@@ -210,8 +192,8 @@ public class BinTree {
 	 * stringifies tree1 and tree2's params and compares tree1's params with tree2's
 	 * params.
 	 * 
-	 * @param {*} tree1 : BinTree object.
-	 * @param {*} tree2 : BinTree object.
+	 * @param tree1 : BinTree object.
+	 * @param tree2 : BinTree object.
 	 */
 	public boolean evaluateEQ(BinTree tree1, BinTree tree2) {
 		if (tree1.equals(tree2))
@@ -222,7 +204,7 @@ public class BinTree {
 	/**
 	 * Checks if a given tree is not equal to nil.
 	 * 
-	 * @param {*} tree : BinTree object.
+	 * @param tree : BinTree object.
 	 */
 	public Boolean isTrue(BinTree tree) {
 		if (tree.getData() != "nil")
--- lib/Libwh.java
+++ lib/Libwh.java
@@ -9,6 +9,9 @@ import compilation.BinTree;
 import java.util.function.Function;
 import javax.script.*;
 import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 /**
  * @author sievi
@@ -22,8 +25,7 @@ public class Libwh {
 	public static BinTree bintree ;
 
 	public Libwh() {
-		// super();
-		// TODO Auto-generated constructor stub
+		//super()
 	}
 
 	public void nop() {
@@ -46,7 +48,7 @@ public class Libwh {
 
 	public BinTree list(BinTree adr2, BinTree adr3) {
 		bintree = new BinTree("nill", adr2, adr3);
-		List<BinTree> args = new ArrayList<BinTree>();
+		LinkedList<BinTree> args = new LinkedList<BinTree>();
 		args.add(adr2); // clone the adr2
 		args.add(adr3); // clone the adr3
 		return bintree.list(args); // return the new bintree
@@ -57,25 +59,20 @@ public class Libwh {
 	}
 
 	public BinTree tl(BinTree adr) {
-		
 		return adr.tail(adr);
 	}
 
 	public BinTree not(BinTree adr2) {
-		//BinTree bintree = new BinTree("nill", null, null);
-		if (adr2.isTrue(adr2)) {
+		bintree = new BinTree("nill", null, null);
+		if (isTrue(adr2)) {
 			return bintree;
 		} else {
-			return new BinTree("cons", new BinTree("nil", null, null), new BinTree("nil", null, null));
+			return new BinTree("cons", bintree, bintree);
 		}
 	}
 
 	public boolean isTrue(BinTree tree) {
-
-		if (tree.getData() != "nil")
-			return true;
-		return false;
-
+		return tree.isTrue(tree);
 	}
 
 	public BinTree and(BinTree adr2, BinTree adr3) {
@@ -84,12 +81,12 @@ public class Libwh {
 	}
 
 	public BinTree or(BinTree adr2, BinTree adr3) {
-		BinTree bintree = new BinTree("OR", null, null);
+		bintree = new BinTree("OR", null, null);
 		return bintree.evaluate("OR", adr2, adr3);
 	}
 
 	public BinTree eq(BinTree adr2, BinTree adr3) {
-		BinTree bintree = new BinTree("EQ", null, null);
+		bintree = new BinTree("EQ", null, null);
 		return bintree.evaluate("EQ", adr2, adr3);
 	}
 
@@ -99,8 +96,8 @@ public class Libwh {
 	}
 
 	/**
-	 * @params : BinTree
-	 * 
+	 * @param : BinTree
+	 * @return String
 	 */
 	public String bintreeToString(BinTree X) {
 		String sortie = "";
@@ -118,20 +115,17 @@ public class Libwh {
 	 * !! : The char sequence should be written as "(cons X Y)" or "(list X Y)" or
 	 * "(nil)" where X and Y are arguments in the same form.
 	 * 
-	 * @param {*} str : String representing a BinTree.
+	 * @param str : String representing a BinTree.
+	 * @return BinTree
 	 */
 	public BinTree bintreeFromString(String val) {
 
-		/* if the parameter is an int, launch public BinTree bintreeFromInt. */
-		String subVal = "";
-		int h = 0;
 		BinTree res = bintreeFromInt(0);
 		try {
 			res = bintreeFromInt(Integer.parseInt(val));
 			return res;
 
 		} catch (Exception e) {
-			// System.out.println("Ce n'est pas un entier");
 		}
 
 		/*
@@ -139,109 +133,59 @@ public class Libwh {
 		 * adequate BinTree.
 		 */
 
-		if (val.charAt(0) == '(') {
+		int j = 0;
+		int k = 0;
 
-			int a = StringBinTree(val, h);
+		// le bintree
+		Pattern p1 = Pattern.compile("([(]cons nil )+nil([)])+");
+		Matcher m1 = p1.matcher(val);
 
-			res = bintreeFromInt(a);
+		// Nombre parenthèse ouvrantes
+		Pattern p2 = Pattern.compile("[(]");
+		Matcher m2 = p2.matcher(val);
 
-			return res;
+		// Nombre parenthèse fermantes
+		Pattern p3 = Pattern.compile("[)]");
+		Matcher m3 = p3.matcher(val);
 
-		} else // we create a symbol
-		{
-			res = new BinTree(val.toString(), null, null);
+		if (m1.matches()) {
+
+			j = 0;
+			k = 0;
+			while (m2.find()) {
+				j = j + 1;
+			}
+			while (m3.find()) {
+				k = k + 1;
+			}
+			if (j == k) {
+				res= bintreeFromInt(j);
+			}
+		}else{
+			System.out.println("bintree mal formate donne nil(0)");
+			res= new BinTree(val.toString(), null, null);
 		}
+		
 		return res;
 	}
-
-	public Integer StringBinTree(String val, int k) {
-		int i = 0;
-		// System.out.println(val.substring(6, 9));
-		if (val.substring(i + 1, i + 5).equals("cons")/* || "list" == val.substring(i + 1, i + 5) */) {
-			if ((val.substring(i + 6, i + 9).equals("nil")) && (!val.substring(10, val.length() - 1).equals("nil"))) {
-				//System.out.println(k);
-				val = val.substring(10, val.length() - 1);
-				k++;
-				k = StringBinTree(val, k);
-			} else if ((val.substring(i + 6, i + 9).equals("nil"))
-					&& (val.substring(10, val.length() - 1).equals("nil"))) {
-				k++;
-			} else {
-				// System.out.println("bintree mal format�");
-				return null;
-			}
-
-		} else {
-			System.out.println("symbole");
-			return null;
-		}
-		return k;
-	}
-
-	/**
-	 * Parsing : Finding the position of the next '(' parenthesis of a char sequence
-	 * representing a BinTree.
-	 * 
-	 * @param {*} str : Char Sequence representing a BinTree object.
-	 * @param {*} position : Integer representing our position in str.
-	 * @returns position of the next '('.
-	 */
-	public Integer getOpenParenthesis(CharSequence str, Integer position) {
-		while (str.charAt(position) != '(' && position <= str.length()) {
-			position++;
-		}
-		if (position > str.length()) {
-			return -1;
-		}
-		return position;
-	};
 
 	public void nonImpl(){
 		
 	}
-	/**
-	 * Parsing : Finding the position of the next ')' parenthesis of a string, from
-	 * a '(' parenthesis.
-	 * 
-	 * @param {*} str : Char Sequence representing a BinTree object.
-	 * @param {*} position : Integer representing the position of the previous
-	 *            opening parenthesis.
-	 * @returns position of the closing parenthesis, if there was an opening
-	 *          parenthesis found previously.
-	 */
-	public Integer getCloseParenthesis(CharSequence str, Integer position) {
-		if (str.charAt(position) != '(') {
-			return -1;
-		}
-		int nbOpenParenthesis = 1;
-		boolean found = false;
-		while (!found) {
-			position++;
-			if (str.charAt(position) == ')') {
-				if (nbOpenParenthesis == 1) {
-					found = true;
-				} else {
-					nbOpenParenthesis--;
-				}
-			} else if (str.charAt(position) == '(') {
-				nbOpenParenthesis++;
-			}
-		}
-		return position;
-	};
+
 
 	/**
 	 * Converts a given BinTree object to an Integer.
 	 * 
-	 * @param {*} tree : BinTree object.
-	 * @returns Integer representing param tree.
+	 * @param tree : BinTree object.
+	 * @return Integer representing param tree.
 	 */
-	public Integer intFromBintree(BinTree tree) {
+	public Integer bintreeToInt(BinTree tree) {
 		int res = 0;
 		if (tree != null) {
 			if (tree.getData() == "cons" || tree.getData() == "list") {
 				res++;
-				res += intFromBintree(tree.getRight());
+				res += bintreeToInt(tree.getRight());
 			}
 			;
 		}
@@ -252,8 +196,8 @@ public class Libwh {
 	/**
 	 * Constructs a bintree from a given Integer.
 	 * 
-	 * @param {*} int : Integer to convert.
-	 * @returns BinTree.
+	 * @param int : Integer to convert.
+	 * @return BinTree.
 	 */
 	public BinTree bintreeFromInt(int a) {
 		BinTree res = new BinTree("nil", null, null);
@@ -280,4 +224,6 @@ public class Libwh {
 //		out=f0(in);
 //		outWH(out);
 	}
+
+	
 }
